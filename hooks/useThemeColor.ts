bfd86d33879afd4371d4084c99e11c17768e1c15
mbarcante <matheusbarcante@hotmail.com>
2024-06-10 20:50:57 -0300

    Checknox + Questions Array

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -5,7 +5,7 @@
 
 import { useColorScheme } from 'react-native';
 
-import { Colors } from '@/constants/Colors';
+import { Colors } from '@/components/constants/Colors';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
